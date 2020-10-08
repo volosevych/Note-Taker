e0c46c51b3be8e52c46f256163a2d645e6bd251b
Anastasia Volosevych <vestyankas@gmail.com>
2020-10-08 12:01:33 -0600

    finished setup express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+fs.readFile("db/db.json", "json", function(data) {
+    var notes = data;
+})
+
+require("routes/routes")(app);
+
+app.listen(PORT, function() {
+    console.log("App lostening on http://localhost:" + PORT);
+})
